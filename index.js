84121cc273f698e830d26bea3faa8d876ffaaf85
barakatimothy <timothybaraka39@gmail.com>
2023-06-11 15:56:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let count = 0; count < names.length; count++) {
+      const message = `Thank you, ${names[count]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+}
+function countDown(){
+    if (count !==0)
+    console.log(count);
+}
